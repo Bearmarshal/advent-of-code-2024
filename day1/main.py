447cad9b106f0b51d21bc5df9dc374420943e38b
3jörn Mårtensson <bearmarshal@gmail.com>
2024-12-01 22:32:24 +0100

    Dag 1: Uppfinn nytt antipattern för enkelradare

--- day1/main.py
+++ day1/main.py
@@ -7,8 +7,7 @@ def part1(filename):
 	print("Part 1: {}".format(sum((abs(int(a) - int(b)) for a, b in zip(*(sorted(location_list) for location_list in zip(*(re.findall(r"(\d+)", line) for line in io.open(filename, mode = 'r')))))))))
 
 def part2(filename):
-	left_list, right_list = zip(*(re.findall(r"(\d+)", line) for line in io.open(filename, mode = 'r')))
-	print("Part 2: {}".format(sum((abs(int(x) * right_list.count(x)) for x in left_list))))
+	print("Part 2: {}".format((lambda left_list, right_list: sum((abs(int(x) * right_list.count(x)) for x in left_list)))(*zip(*(re.findall(r"(\d+)", line) for line in io.open(filename, mode = 'r'))))))
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:

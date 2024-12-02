59181c6f0c21ab234b85c8daf226e5d3ef0ecfa4
3jörn Mårtensson <bearmarshal@gmail.com>
2024-12-02 03:57:22 +0100

    Dag 1: Gör del 1 golfigare utan att tulla på variabelnamnen

--- day1/main.py
+++ day1/main.py
@@ -4,7 +4,7 @@ import re
 import sys
 
 def part1(filename):
-	print("Part 1: {}".format(sum((abs(int(a) - int(b)) for a, b in zip(*(sorted(location_list) for location_list in zip(*(re.findall(r"(\d+)", line) for line in io.open(filename, mode = 'r')))))))))
+	print("Part 1: {}".format(sum(abs(left - right) for left, right in zip(*map(sorted, zip(*(map(int, re.findall(r"(\d+)", line)) for line in io.open(filename, mode = 'r'))))))))
 
 def part2(filename):
 	print("Part 2: {}".format((lambda left_list, right_list: sum((abs(int(x) * right_list.count(x)) for x in left_list)))(*zip(*(re.findall(r"(\d+)", line) for line in io.open(filename, mode = 'r'))))))

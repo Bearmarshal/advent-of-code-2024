e397b9ff4c7790b72bc76c5aec7bdd7f2797e306
3jörn Mårtensson <bearmarshal@gmail.com>
2024-12-03 09:08:01 +0100

    Dag 3: Gör en enkelradare av del 2

--- day3/main.py
+++ day3/main.py
@@ -1,3 +1,4 @@
+import functools
 import io
 import itertools
 import os
@@ -8,16 +9,7 @@ def part1(filename):
 	print("Part 1: {}".format(sum(int(a) * int(b) for line in io.open(filename, mode = 'r') for a, b in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", line))))
 
 def part2(filename):
-	enabled = True
-	total = 0
-	for match in re.finditer(r"(?P<mul>mul\((?P<a>\d{1,3}),(?P<b>\d{1,3})\))|(?P<do>do\(\))|(?P<dont>don't\(\))", io.open(filename, mode = 'r').read()):
-		if match["mul"] and enabled:
-			total += int(match["a"]) * int(match["b"])
-		elif match["do"]:
-			enabled = True
-		elif match["dont"]:
-			enabled = False
-	print("Part 2: {}".format(total))
+	print("Part 2: {}".format(functools.reduce(lambda acc, match: (acc[0], True) if match["do"] else (acc[0], False) if match["dont"] else (acc[0] + int(match["a"]) * int(match["b"]), True) if acc[1] else acc, re.finditer(r"(?P<mul>mul\((?P<a>\d{1,3}),(?P<b>\d{1,3})\))|(?P<do>do\(\))|(?P<dont>don't\(\))", io.open(filename, mode = 'r').read()), (0, True))[0]))
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:

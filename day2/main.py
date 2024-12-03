f2ffe6759e0e305445ee9abf976fc7df84da6be9
3jörn Mårtensson <bearmarshal@gmail.com>
2024-12-03 08:46:18 +0100

    Dag 2: Lite städning

--- day2/main.py
+++ day2/main.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 def part1(filename):
-	print("Part 1: {}".format([all(n - p in (1, 2, 3) for p, n in itertools.pairwise(report)) or all(n - p in (-1, -2, -3) for p, n in itertools.pairwise(report)) for report in (list(map(int, re.findall(r"(\d+)", line))) for line in io.open(filename, mode = 'r'))].count(True)))
+	print("Part 1: {}".format([all(n - p in (1, 2, 3) for p, n in level_pairs) or all(n - p in (-1, -2, -3) for p, n in level_pairs) for level_pairs in (list(itertools.pairwise(map(int, re.findall(r"(\d+)", line)))) for line in io.open(filename, mode = 'r'))].count(True)))
 
 def part2(filename):
 	print("Part 2: {}".format([any(all(n - p in (1, 2, 3) for p, n in itertools.pairwise(alt_report)) or all(n - p in (-1, -2, -3) for p, n in itertools.pairwise(alt_report)) for alt_report in [report] + [report[0:i] + report[i+1:len(report)] for i in range(len(report))]) for report in (list(map(int, re.findall(r"(\d+)", line))) for line in io.open(filename, mode = 'r'))].count(True)))

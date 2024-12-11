559bbfd091508628eff89947f7d80666b9784f7c
3jörn Mårtensson <bearmarshal@gmail.com>
2024-12-11 12:50:37 +0100

    Dag 11: Dynamisk programmering i stället för naiv länkad lista

--- day11/main.py
+++ day11/main.py
@@ -1,55 +1,39 @@
+import collections
 import io
 import os
 import re
 import sys
 
-class Pebble:
-	def __init__(self, value, next=None):
-		self.value = value
-		self.next = next
-
-	def __iter__(self):
-		current = self
-		while current:
-			yield current
-			current = current.next
-
-	def add_after(self, value):
-		self.next = Pebble(value, self.next)
-
-	def blink(self):
-		current = self
-		while current:
-			next = current.next
-			if current.value == 0:
-				current.value = 1
-			elif (num_digits := len(str(current.value))) % 2 == 0:
-				current.add_after(current.value % 10 ** (num_digits // 2))
-				current.value //= 10 ** (num_digits // 2)
-			else:
-				current.value *= 2024
-			current = next
-
-	def from_list(values):
-		head_pebble = None
-		for value in reversed(values):
-			head_pebble = Pebble(value, head_pebble)
-		return head_pebble
-
+def blink(value):
+	if value == 0:
+		return [1]
+	elif (num_digits := len(str(value))) % 2 == 0:
+		denominator = 10 ** (num_digits // 2)
+		return [value % denominator, value // denominator]
+	else:
+		return [value * 2024]
 
 def part1(filename):
 	with io.open(filename, mode = 'r') as file:
-		pebbles = Pebble.from_list([int(x) for x in re.findall("\d+", file.read())])
+		pebbles = collections.Counter([int(x) for x in re.findall("\d+", file.read())])
 	for _ in range(25):
-		pebbles.blink()
-	print("Part 1: {}".format(len(list(pebbles))))
+		new_pebbles = collections.Counter()
+		for value, count in pebbles.items():
+			for new_value in blink(value):
+				new_pebbles[new_value] += count
+		pebbles = new_pebbles
+	print("Part 1: {}".format(sum(pebbles.values())))
 
 def part2(filename):
 	with io.open(filename, mode = 'r') as file:
-		pebbles = Pebble.from_list([int(x) for x in re.findall("\d+", file.read())])
+		pebbles = collections.Counter([int(x) for x in re.findall("\d+", file.read())])
 	for _ in range(75):
-		pebbles.blink()
-	print("Part 2: {}".format(len(list(pebbles))))
+		new_pebbles = collections.Counter()
+		for value, count in pebbles.items():
+			for new_value in blink(value):
+				new_pebbles[new_value] += count
+		pebbles = new_pebbles
+	print("Part 2: {}".format(sum(pebbles.values())))
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:

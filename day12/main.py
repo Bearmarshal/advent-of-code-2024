4de69faf7f116fa3f7b09f561db5d906ec5b88ef
3jörn Mårtensson <bearmarshal@gmail.com>
2024-12-13 14:39:45 +0100

    Dag 12: Använd `aoclib.posdir`

--- day12/main.py
+++ day12/main.py
@@ -1,86 +1,12 @@
 import collections
-import enum
 import io
 import itertools
 import os
 import re
 import sys
 
-class EnumContainsValueMeta(enum.EnumMeta):
-	def __contains__(cls, value):
-		return value in cls.__members__.values()
-
-class Direction(tuple):
-	def __new__(cls, dy, dx):
-		self = tuple.__new__(cls, (dy, dx))
-		self.dy = dy
-		self.dx = dx
-		return self
-
-	def __neg__(self):
-		return Direction(-self.dy, -self.dx)
-
-	def __add__(self, other):
-		return Direction(self.dy + other.dy, self.dx + other.dx)
-	
-	def __sub__(self, other):
-		return Direction(self.dy - other.dy, self.dx - other.dx)
-	
-	def __mul__(self, value):
-		return Direction(self.dy * value, self.dx * value)
-	
-	def __rmul__(self, value):
-		return self.__mul__(value)
-	
-class CardinalDirection(Direction, enum.Enum):
-	def __new__(cls, dy, dx):
-		self = Direction.__new__(cls, dy, dx)
-		self._value_ = (dy, dx)
-		return self
-
-	def __neg__(self):
-		return CardinalDirection((-self.dy, -self.dx))
-
-	def opposite(self):
-		return -self
-
-	def right(self):
-		return CardinalDirection((self.dx, -self.dy))
-
-	def left(self):
-		return CardinalDirection((-self.dx, self.dy))
-	
-	def __str__(self):
-		return self.name
-
-	@classmethod
-	def from_glyph(cls, glyph):
-		match glyph:
-			case "^": return cls.NORTH
-			case "v": return cls.SOUTH
-			case "<": return cls.WEST
-			case ">": return cls.EAST
-
-	NORTH = (-1, 0)
-	SOUTH = (1, 0)
-	WEST = (0, -1)
-	EAST = (0, 1)
-
-class Position(tuple):
-	def __new__(cls, y, x):
-		self = tuple.__new__(cls, (y, x))
-		self.y = y
-		self.x = x
-		return self
-
-	def __add__(self, direction: Direction):
-		return Position(self.y + direction.dy, self.x + direction.dx)
-	
-	def __sub__(self, other):
-		return Direction(self.y - other.y, self.x - other.x)
-
-	def get_manhattan_distance(self, other):
-		return abs(self.y - other.y) + abs(self.x - other.x)
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from aoclib.posdir import Position, CardinalDirection
 
 
 def part1(filename):

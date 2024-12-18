3ffce3490f799201023a63cb5bdc0b43744dd648
3jörn Mårtensson <bearmarshal@gmail.com>
2024-12-18 08:06:47 +0100

    Dag 18: Optimera del 2

--- day18/main.py
+++ day18/main.py
@@ -38,7 +38,6 @@ def part1(filename):
 	closed_set = {}
 	while open_set.qsize():
 		prioritised_item = open_set.get()
-		heuristic = prioritised_item.priority
 		position, distance = prioritised_item.item
 		if position in closed_set:
 			continue
@@ -57,34 +56,36 @@ def part2(filename):
 	
 	memory_dim_y = range(71)
 	memory_dim_x = range(71)
-	memory_map = [["." for _ in memory_dim_x] for _ in memory_dim_y]
 	start = Position(0, 0)
 	end = Position(70, 70)
 
-	for memory_write in memory_writes[:1024]:
-		memory_write[memory_map] = "#"
-
-	for memory_write in memory_writes[1024:]:
-		memory_write[memory_map] = "#"
+	last_succesful = 1023
+	first_failed = len(memory_writes)
+	while first_failed - last_succesful > 1:
+		i = (last_succesful + first_failed) // 2
+		memory_map = [["." for _ in memory_dim_x] for _ in memory_dim_y]
+		for memory_write in memory_writes[:i + 1]:
+			memory_write[memory_map] = "#"
 		open_set = queue.PriorityQueue()
 		open_set.put(PrioritisedItem(start.get_manhattan_distance(end), (start, 0)))
 		closed_set = {}
 		while open_set.qsize():
 			prioritised_item = open_set.get()
-			heuristic = prioritised_item.priority
 			position, distance = prioritised_item.item
 			if position in closed_set:
 				continue
 			closed_set[position] = distance
 			if position == end:
+				last_succesful = i
 				break
 			for direction in CardinalDirection:
 				neighbour = position + direction
 				if neighbour.y in memory_dim_y and neighbour.x in memory_dim_x and neighbour[memory_map] != "#" and neighbour not in closed_set:
 					open_set.put(PrioritisedItem(distance + 1 + neighbour.get_manhattan_distance(end), (neighbour, distance + 1)))
 		else:
-			break
-	print("Part 2: {},{}".format(memory_write.x, memory_write.y))
+			first_failed = i
+	first_failed_write = memory_writes[first_failed]
+	print("Part 2: {},{}".format(first_failed_write.x, first_failed_write.y))
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:

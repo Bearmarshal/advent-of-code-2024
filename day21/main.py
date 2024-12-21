97983035bd9450110f6a953a18195f2e528d8bbd
3jörn Mårtensson <bearmarshal@gmail.com>
2024-12-21 18:29:43 +0100

    Dag 21: Del 2 är snabb nu men ger fel svar, oklart varför

--- day21/main.py
+++ day21/main.py
@@ -10,7 +10,7 @@ import sys
 import time
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from aoclib.posdir import Direction, Position, CardinalDirection
+from aoclib.posdir import *
 from aoclib.tilemap import TileMap
 import aoclib.inputreader as inputreader
 
@@ -59,52 +59,70 @@ def part1(filename):
 def sequence_to_string(sequence):
 	return "".join(CardinalDirection.glyph(direction) if isinstance(direction, Direction) else direction for direction in sequence)
 
-def shortest_sequence(sequence, robots, keypads, keypad_buttons, cache = {}, cache2 = {}):
+def shortest_sequence_length(sequence, robots, keypads, keypad_sequences, cache = {}):
 	if not robots:
-		return sequence
+		return len(sequence)
 	robot, *robots = robots
-	if (len(robots), sequence_to_string(sequence)) in cache:
-		return cache[(len(robots), sequence_to_string(sequence))]
+	if (len(robots), sequence) in cache:
+		return cache[(len(robots), sequence)]
 	keypad, *keypads = keypads
-	shortest_total_sequence = []
+	sequence_map = keypad_sequences[keypad]
+	shortest_total_sequence_length = 0
 	for button in sequence:
-		shortest_button_sequence = None
-		if (robot, keypad_buttons[keypad][button]) in cache2:
-			robot_sequences = cache2[(robot, keypad_buttons[keypad][button])]
-		else:
-			robot_sequences = keypad.find_all_shortest_paths(robot, keypad_buttons[keypad][button], neighbours=lambda self, position: [neighbour for direction in CardinalDirection if (neighbour := position + direction) in self and keypad[neighbour] != " "])
-			cache2[(robot, keypad_buttons[keypad][button])] = robot_sequences
-		for robot_sequence in robot_sequences:
-			robot_directions = [to_button - from_button for from_button, to_button in itertools.pairwise(robot_sequence)] + ["A"]
-			current_button_sequence = shortest_sequence(robot_directions, robots, keypads, keypad_buttons, cache)
-			if shortest_button_sequence is None or len(current_button_sequence) < len(shortest_button_sequence):
-				shortest_button_sequence = current_button_sequence
-		if shortest_button_sequence is None:
-			return None
-		shortest_total_sequence += shortest_button_sequence
-		robot = keypad_buttons[keypad][button]
-	cache[(len(robots), sequence_to_string(sequence))] = shortest_total_sequence
-	return shortest_total_sequence
+		robot_sequence = sequence_map[robot][button]
+		shortest_total_sequence_length += shortest_sequence_length(robot_sequence, robots, keypads, keypad_sequences, cache)
+		robot = button
+	cache[(len(robots), sequence)] = shortest_total_sequence_length
+	return shortest_total_sequence_length
 
 def part2(filename):
 	with io.open(filename, mode = 'r') as file:
 		codes = [line.strip() for line in file if line.strip()]
 
 	numpad = TileMap(["789", "456", "123", " 0A"])
-	dirpad = TileMap([[" ", CardinalDirection.NORTH, "A"], [CardinalDirection.WEST, CardinalDirection.SOUTH, CardinalDirection.EAST]])
+	dirpad = TileMap([" ^A", "<v>"])
 
 	num_buttons = { button_num: position for y in numpad.y_dim for x in numpad.x_dim if (button_num := numpad[position := Position(y, x)]) != " " }
 	dir_buttons = { button_num: position for y in dirpad.y_dim for x in dirpad.x_dim if (button_num := dirpad[position := Position(y, x)]) != " " }
 
+	numpad_sequences = {button: {
+		other: "".join(direction.glyph() for direction in sorted((num_buttons[other] - num_buttons[button]).discretise(), key=lambda direction: (WEST, SOUTH, EAST, NORTH).index(direction))) + "A" for other in "A0123456789"
+		} for button in "A0123456789"}
+	numpad_sequences["0"]["1"] = "^<A"
+	numpad_sequences["0"]["4"] = "^^<A"
+	numpad_sequences["0"]["7"] = "^^^<A"
+	numpad_sequences["A"]["1"] = "^<<A"
+	numpad_sequences["A"]["4"] = "^^<<A"
+	numpad_sequences["A"]["7"] = "^^^<<A"
+	numpad_sequences["1"]["0"] = ">vA"
+	numpad_sequences["1"]["A"] = ">>vA"
+	numpad_sequences["4"]["0"] = ">vvA"
+	numpad_sequences["4"]["A"] = ">>vvA"
+	numpad_sequences["7"]["0"] = ">vvvA"
+	numpad_sequences["7"]["A"] = ">>vvvA"
+	dirpad_sequences = {
+		"A": {"A":    "A", "^":  "<A", "v": "<vA", "<": "v<<A", ">":  "vA" },
+		"^": {"A":   ">A", "^":   "A", "v":  "vA", "<":  "v<A", ">": "v>A" },
+		"v": {"A":  "^>A", "^":  "^A", "v":   "A", "<":   "<A", ">":  ">A" },
+		"<": {"A": ">>^A", "^": ">^A", "v":  ">A", "<":    "A", ">": ">>A" },
+		">": {"A":   "^A", "^": "<^A", "v":  "<A", "<":  "<<A", ">":   "A" },
+		}
+
+	for button in "A0123456789":
+		print(f"{button}: [", " | ".join(f"{other}: {numpad_sequences[button][other]:>6}" for other in "A0123456789"), "]")
+	print()
+	for button in "A^<v>":
+		print(f"{button}: [", " | ".join(f"{other}: {dirpad_sequences[button][other]:>6}" for other in "A^<v>"), "]")
+
 	keypads = [numpad] + 25 * [dirpad]
-	keypad_buttons = { numpad: num_buttons, dirpad: dir_buttons }
-	robots = [keypad_buttons[keypad]["A"] for keypad in keypads]
+	keypad_sequences = { numpad: numpad_sequences, dirpad: dirpad_sequences }
+	robots = ["A" for _ in keypads]
 
 	complexity = 0
 	cache = {}
-	cache2 = {}
 	for code in codes:
-		complexity += int(code[:-1]) * len(shortest_sequence(code, robots, keypads, keypad_buttons, cache, cache2))
+		print(shortest_sequence_length(code, robots, keypads, keypad_sequences, cache))
+		complexity += int(code[:-1]) * shortest_sequence_length(code, robots, keypads, keypad_sequences, cache)
 
 	print("Part 2: {}".format(complexity))
 
